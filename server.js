f2b7dfecdf1792c841de9d90746d3bcb1f764aa1
Anne Bellefleur <annebellefleur@Annes-Air.attlocal.net>
2020-04-30 13:17:34 -0400

    server.js has all routes and all required files and middleware

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const venueRouter = require('./controllers/venue.js')
+const vendorRouter = require('./controllers/vendor.js')
+const reviewRouter = require('./controllers/review.js')
+const blogRouter = require('./controllers/blog.js')
+
+
+
+const methodOverride = require('method-override')
+
+const app = express()
+
+
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(methodOverride('_method'))
+app.use(express.static(__dirname + '/public'))
+
+app.use('/venue', venueRouter)
+app.use('/vendor', vendorRouter)
+app.use('/review', reviewRouter)
+app.use('/blog', blogRouter)
+
+
+app.set('view engine', 'hbs')
+
+
+
+
+
+
+const port = 3000
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+})
+
+//WORKING

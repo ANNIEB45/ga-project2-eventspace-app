56b9331e2e87d44b3faabadc13d72e5d63483bd6
Anne Bellefleur <annebellefleur@Annes-Air.attlocal.net>
2020-05-04 23:04:14 -0400

    removed unused codes. window event listener not working

--- public/script.js
+++ public/script.js
@@ -1,52 +1,40 @@
 // Modal
 // Get the modal
 const modal = document.querySelector("#myModal")
+
 console.log(modal)  //showing as null in console
 // Get the button that opens the modal
 const button = document.querySelectorAll(".myBtn")[0]
 console.log(button) //nodelist with one item = RIGHT
 
 // Get the close button
-const closeBtn = document.getElementsByClassName("close-btn")[0]
-console.log(closeBtn) //empty HTMLCollection LIst
+const closeBtnTop = document.querySelector(".close-btn-topOne")
+console.log(closeBtnTop)
 
-// error message: script.js:14 
-    // Uncaught TypeError: button.addEventListener 
-    // is not a function
+const closeBtn = document.getElementsByClassName("close-btn")[0]
+console.log(closeBtn) 
+
+const deleteBtn = document.querySelector('.delete-btn ')
+
+
 
 button.addEventListener('click', () =>{
     modal.style.display = 'block'
     console.log("you clicked me")
 })
 
+closeBtnTop.addEventListener('click', () =>{
+    modal.style.display = 'none'
+    console.log("you clicked me")
+})
+
 closeBtn.addEventListener('click', () =>{
     modal.style.display = 'none'
     console.log("you clicked me")
-
-
 })
  
-window.addEventListener('click', (e) =>{
-    if(e.target === modal){
-        modal.display.style = 'none'
+window.addEventListener = ('click', (event) => {
+    if (event.target == modal) {
+        modal.style.display = "none";
     }
-    console.log("you clicked me")
-
 })
-
-// for price input field
-// let currencyFormat = document.getElementById('currency').value 
-
-// // function priceInput() {
-// //     if(currencyFormat.indexOf("$") != 0){
-// //         currencyFormat += "$"
-// //     }
-// //     document.getElementById('currency').value = currencyFormat
-// // }
-
-// currencyFormat.addEventLister('change', () =>{
-//     if(currencyFormat.indexOf("$") != 0){
-//         currencyFormat += "$"
-//     }
-//     document.getElementById('currency').value = currencyFormat
-// })
